0d9d76f671801da88ed8aa3e7c64a89db78dfd3d
Franmicar <franmicarrasquilla@gmail.com>
2026-05-04 13:55:52 +0200

    fix(auth): add google provider to capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ras.impostorapp',
   appName: 'Deceptra',
-  webDir: 'dist/impostor-frontend/browser'
+  webDir: 'dist/impostor-frontend/browser',
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ["google.com"],
+    },
+  },
 };
 
 export default config;
